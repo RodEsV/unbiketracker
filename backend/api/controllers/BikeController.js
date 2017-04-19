f88973998fdcbcfef2d615910ac620d62064023b
Edward Carrillo <eccarrilloe@gmail.com>
2017-04-19 16:37:19 -0500

    feat(backend): backend implemented

---
+++ backend/api/controllers/BikeController.js
@@ -0,0 +1,11 @@
+/**
+ * BikeController
+ *
+ * @description :: Server-side logic for managing bikes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ backend/api/controllers/UserController.js
@@ -0,0 +1,24 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	find: function(req, res) {
+  },
+
+  create: function() {
+
+  },
+
+  update: function() {
+
+  },
+
+  destroy: function() {
+
+  }
+};
+
